efe3d165164067a30910fc2256935a83cd634f87
AndreevaMA <maria_andrews89@mail.ru>
2022-11-28 16:21:04 +0300

    Task Solution: business-logic added

---
+++ Csharp_task_solution/Solution.cs
@@ -0,0 +1,17 @@
+﻿// Final task at 1Q developer course
+
+
+//------------------------------------Program start------------------------------------
+int size = UserInput("Input an integer from 5 to 10: ", "Input error!");
+Console.WriteLine($"Now an array with size = {size} will be created");
+
+string[] initArray = FillArray(size);
+PrintArray(initArray);
+
+Console.WriteLine();
+Console.WriteLine($"The new array will consist only of those elements which symbols quantity <= 3: ");
+
+string[] newArray = CreateArray(initArray);
+PrintArray(newArray);
+
+//-------------------------------------Program end-------------------------------------
